9ae00eca5f98cfa42a88ce093159475a8a06aec3
jocelyn11111010111 <yunchenlin818@gmail.com>
2024-05-22 01:31:13 +0800

    Update ani.js

--- ani.js
+++ ani.js
@@ -1,10 +1,12 @@
 function submitHandler(){
     var to = "yunchenlin818@gmail.com";
     var subject = "feedback";
+    var bodyText = document.getElementById('bodyText').value; // 获取 bodyText 的值
     var body = ""+'%0A%0A%0A'
-        body += "From:"+'%0A';
-        body += "message:"+bodyText.value+'%0A';
+    body += "From:"+'%0A';
+    body += "message:"+bodyText.value+'%0A';
 
+    var mailTo = document.getElementById('mailTo');
     mailTo.href="mailto:"+to+"?subject="+subject+"&body="+body;
     mailTo.click();
 }
