f77a939badf9bf555f39aeb2a129c295f2aa7a46
jocelyn11111010111 <yunchenlin818@gmail.com>
2024-06-09 19:45:29 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,10 @@
+document.querySelectorAll('.scroll-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
